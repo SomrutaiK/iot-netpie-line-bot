4be88979b524825c74ba1781dc75d60d535641c8
นินิว สมฤทัย Planning Manager <68342214+SomrutaiK@users.noreply.github.com>
2021-03-10 20:05:51 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'ePz4ODvyPiigAyspRjzAP5q/NxWi+SdbqyS9fM1Tq6DhZP1t15LLXRWY5VtHqyOi6qDnsDd2C5rwcwWeAdpe3nZNvgwuawtdfGqF4Yg39JqPWPkPv//n0FyjAe+FFchq1nYQwR7gajmXK7uMs3A3eAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
